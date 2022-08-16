29d4385e31582344c1501ecbcf9707418484a94e
michael <forgetscode@gmail.com>
2022-08-16 16:32:47 -0700

    api

---
+++ components/Banner.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Movie } from '../typings'
+
+interface Props {
+    netflixOriginals?: Movie[]
+}
+
+function Banner({netflixOriginals}: Props) {
+  return (
+    <div>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Banner
