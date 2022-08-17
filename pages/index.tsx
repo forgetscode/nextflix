86db7fc47b2302f30f8968a0fd56e85bfc5c0e15
michael <forgetscode@gmail.com>
2022-08-16 18:05:06 -0700

    section rows

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import Row from '../components/Row'
 import { Movie } from '../typings'
 import requests from '../utils/requests'
 
@@ -17,7 +18,16 @@ interface Props {
   documentaries: Movie[]
 }
 
-const Home = ( {netflixOriginals}:Props ) => {
+const Home = ( {
+    netflixOriginals,
+    actionMovies,
+    comedyMovies,
+    documentaries,
+    horrorMovies,
+    romanceMovies,
+    topRated,
+    trendingNow,
+  }:Props ) => {
 
   return (
     <div className='relative h-screen bg-gradient-to-b from-gray-900/10
@@ -30,6 +40,13 @@ const Home = ( {netflixOriginals}:Props ) => {
       <main className='relative pl-4 pb-24 lg:space-y-24 lg:pl-16'>
         <Banner netflixOriginals={netflixOriginals}/>
         <section>
+          <Row title="Trending Now" movies={trendingNow} />
+          <Row title="Top Rated" movies={topRated} />
+          <Row title="Action Thrillers" movies={actionMovies} />
+          <Row title="Comedies" movies={comedyMovies} />
+          <Row title="Scary Movies" movies={horrorMovies} />
+          <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Documentaries" movies={documentaries} />
         </section>
       </main>
     </div>
